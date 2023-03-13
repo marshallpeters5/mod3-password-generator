7974b6168305d715a3cb2ffd14db36f136fe9ed1
Marshall <marshall.peters01@gmail.com>
2023-03-13 14:40:20 -0400

    removed assignment comments

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,3 @@
-// Assignment Code
 var uppercaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 var lowercaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var symbols = ["!","@","#","$","%","^","&","*","=","+"]
@@ -39,11 +38,11 @@ function generatePassword() {
   }
   return finalPassword.join("")
 }
-// Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-// Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
