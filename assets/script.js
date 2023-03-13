f60ac724ed039069ca4df8eec871858cab93e3b1
Marshall <marshall.peters01@gmail.com>
2023-03-13 13:23:33 -0400

    added password options

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,9 @@ var lowercaseLetters = ["a", "b","c"]
 var symbols = ["!", "@", "#"]
 var numbers = ["1", "2", "3"]
 var generateBtn = document.querySelector("#generate");
+
 function generatePassword() {
+  var passwordOptions = []
   var length = prompt("How long would you like your password to be?")
   if (length < 8 || length > 128) {
     alert ("Please choose an appropriate length for your password.")
@@ -14,6 +16,18 @@ function generatePassword() {
   var hasLc = confirm("Confirm if you would like to include lowercase letters?")
   var hasSym = confirm("Confirm if you would like to include symbols?")
   var hasNum = confirm("Confirm if you would like to include numbers?")
+  if (hasUc === true) {
+    passwordOptions = passwordOptions.concat (uppercaseLetters)
+  }
+  if (hasLc === true) {
+    passwordOptions = passwordOptions.concat (lowercaseLetters)
+  }
+  if (hasSym === true) {
+    passwordOptions = passwordOptions.concat (symbols)
+  }
+  if (hasNum === true) {
+    passwordOptions = passwordOptions.concat (numbers)
+  }
 }
 // Write password to the #password input
 function writePassword() {
